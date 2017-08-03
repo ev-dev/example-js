93837f7dc985701f4b44ee5b2d5eb1644acfee0b
ev-dev <evan.reed1100@gmail.com>
2017-08-02 21:58:43 -0400

    initial server now working

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const { resolve } = require('path')
+
+// const pkg = require('../package.json')
+
+const app = express()
+
+app.use(require('volleyball'))
+
+module.exports = app
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(bodyParser.json())
+  .use(express.static(resolve(__dirname, '..', 'public'))) // Serve static files from ../public
+  // .use('/api', require('./server/api')) // Serve our api
+  .get('/*', (_, res) => res.sendFile(resolve(__dirname, 'public', 'index.html'))) // Send index.html for any other requests.
+
+if (module === require.main) {
+  const server = app.listen(
+    process.env.PORT || 3000,
+    () => {
+      console.log('--- Started HTTP Server for ExampleJS ---')
+      console.log(`Listening on ${JSON.stringify(server.address())}`)
+    }
+  )
+}
