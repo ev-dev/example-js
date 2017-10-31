f9ddc80e0ea31e7818b4c9040257509486e3d4d1
ev-dev <evan.reed1100@gmail.com>
2017-10-31 14:11:25 -0400

    Feat: Seeding for new models

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,14 +1,12 @@
 import { STRING, BOOLEAN, VIRTUAL } from 'sequelize'
+import crypto from 'crypto'
 import db from '../db'
 
 const User = db.define('user', {
-  alias: {
+  username: {
     type: STRING,
-    allowNull: false
-  },
-  fullname: {
-    type: STRING,
-    allowNull: false
+    allowNull: false,
+    unique: true
   },
   email: {
     type: STRING,
@@ -18,17 +16,43 @@ const User = db.define('user', {
       isEmail: true
     }
   },
-  password_digest: STRING,
-
   password: VIRTUAL,
-
+  salt: STRING,
+  googleId: STRING,
+  githubId: STRING,
+  fullname: {
+    type: STRING,
+    allowNull: false
+  },
+  profileImgUrl: STRING,
   organization: STRING,
-
   isPublic: {
     type: BOOLEAN,
     defaultValue: true
   }
 })
 
-export default User
+User.prototype.checkPswd = function(candidate) {
+  return this.password === User.encryptPassword(candidate, this.salt)
+}
 
+User.generateSalt = () => crypto
+  .randomBytes(16).toString('base64');
+
+User.encryptPassword = (plainText, salt) => crypto
+  .createHash('RSA-SHA256')
+  .update(plainText)
+  .update(salt)
+  .digest('hex')
+
+const setSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt()
+    user.password = User.encryptPassword(user.password, user.salt)
+  }
+}
+
+User.beforeCreate(setSaltAndPassword)
+User.beforeUpdate(setSaltAndPassword)
+
+export default User
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,26 +1,42 @@
 import _ from 'lodash'
-import casual from 'casual'
-import { blue } from 'chalk'
+import faker from 'faker'
+import { blue, green, red } from 'chalk'
 import { db, User, Post } from './models'
 
-export default () => {
+const seed = () => {
   console.log(blue('\n  - Seeding Database...'))
-  casual.seed(123)
+  faker.seed(123)
 
   return db.sync({ force: true })
     .then(() =>
-      _.times(10, () =>
-        User.create({
-          name: casual.first_name
+      _.times(20, () => {
+        const userFirst = faker.name.firstName()
+        const userLast = faker.name.lastName()
+
+        return User.create({
+          username: faker.internet.userName(userFirst, userLast),
+          email: faker.internet.email(userFirst, userLast),
+          password: faker.internet.password(),
+          fullname: `${userFirst} ${userLast}`,
+          profileImgUrl: faker.image.avatar(),
+          organization: faker.company.companyName()
         })
-          .then(createdUser =>
-            createdUser.createPost({
-              description: `Post by ${createdUser.name}  ---------  ${casual.sentences(3)}`,
-              imageUrl: casual.url,
-              votes: casual.integer(0, 1000)
+        .then(createdUser =>
+          createdUser.createExample({
+            snippet: faker.lorem.paragraph(),
+            stars: faker.random.number(150)
+          }))
+          .then(createdExample => 
+            createdExample.createComment({
+              content: faker.lorem.text()
             }))
-      ))
+      }))
+    .then(() => {
+      console.log(green.bold('\n  - Seeding Complete.\n'))
+    })
     .catch(err => {
-      console.error('Error...Problem Seeding DB.')
+      console.error(red('\n  - Error...Problem Seeding DB.\n\n'), err)
     })
 }
+
+seed()
