dd50b7b802c24beb91efadb2d944d640821846da
ev-dev <evan.reed1100@gmail.com>
2017-11-06 22:01:04 -0500

    Fix: bad character typo in Search.jsx

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -27,7 +27,7 @@ class MainSearch extends Component {
     const uri = this.props.location.pathname
     this.setState({ isHomepage: uri === '/' })
 
-    this.props.history.pu]sh(
+    this.props.history.push(
       this.state.isHomepage
         ? `/results/${query}/mdn/1`
         : `/results/${query}/${src}/${id}`
